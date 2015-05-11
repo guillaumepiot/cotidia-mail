2b87569b7b7897f77866c0eea4ecf4ad9ca012db
Guillaume Piot <guillaume@cotidia.com>
2015-05-11 19:17:03 +0100

    Updated dependencies

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ CLASSIFIERS = [
 install_requires = [
     'djrill==1.3.0',
     'cssutils==0.9.10',
-    'django-form-utils==1.0.1'
+    'django-form-utils==1.0.2'
 ]
 
 # taken from django-registration
