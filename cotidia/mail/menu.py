028149d9569fc12ca7dad33cefac75be77bf763a
Guillaume Piot <guillaume@cotidia.com>
2018-01-02 18:20:09 +0000

    Add menu permissions.

--- cotidia/mail/menu.py
+++ cotidia/mail/menu.py
@@ -7,14 +7,18 @@ def admin_menu(context):
             "text": "Notices",
             "url": reverse("mail-admin:list"),
             "icon": "envelope",
+            "permissions": [
+                "mail.add_emaillog",
+                "mail.change_emaillog"
+            ],
         },
         {
             "text": "Logs",
             "url": reverse("mail-admin:logs"),
             "icon": "file-text-o",
             "permissions": [
-                "perms.mail.add_emaillog",
-                "perms.auth.change_add_emaillog"
+                "mail.add_emaillog",
+                "mail.change_emaillog"
             ],
         }
     ]
