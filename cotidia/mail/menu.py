3f9b0e8806d4fa54e8e9ea48de488f881cf56448
Guillaume Piot <guillaume@cotidia.com>
2018-11-02 18:20:13 +0000

    Update menu.

--- cotidia/mail/menu.py
+++ cotidia/mail/menu.py
@@ -4,21 +4,28 @@ from django.urls import reverse
 def admin_menu(context):
     return [
         {
-            "text": "Notices",
-            "url": reverse("mail-admin:list"),
             "icon": "envelope",
-            "permissions": [
-                "mail.add_emaillog",
-                "mail.change_emaillog"
-            ],
-        },
-        {
-            "text": "Logs",
-            "url": reverse("mail-admin:logs"),
-            "icon": "file-alt",
-            "permissions": [
-                "mail.add_emaillog",
-                "mail.change_emaillog"
-            ],
+            "text": "Email",
+            "description": "See email template and logs.",
+            "nav_items": [
+                {
+                    "text": "Notices",
+                    "url": reverse("mail-admin:list"),
+                    "icon": "envelope",
+                    "permissions": [
+                        "mail.add_emaillog",
+                        "mail.change_emaillog"
+                    ],
+                },
+                {
+                    "text": "Logs",
+                    "url": reverse("mail-admin:logs"),
+                    "icon": "file-alt",
+                    "permissions": [
+                        "mail.add_emaillog",
+                        "mail.change_emaillog"
+                    ],
+                }
+            ]
         }
     ]
