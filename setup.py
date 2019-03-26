bb1ce5957967fffd87aade2be8b216f51d2a00e3
Guillaume Piot <guillaume@cotidia.com>
2019-03-26 11:47:02 +0000

    Update setup.

--- setup.py
+++ setup.py
@@ -10,24 +10,20 @@ setup(
     url="https://code.cotidia.com/cotidia/mail/",
     packages=find_packages(),
     package_data={
-        'cotidia.mail': [
-            'templates/admin/mail/*.html',
-            'templates/notice/*.html',
-            'templates/notice/*.txt',
+        "cotidia.mail": [
+            "templates/admin/mail/*.html",
+            "templates/notice/*.html",
+            "templates/notice/*.txt",
         ]
     },
-    namespace_packages=['cotidia'],
+    namespace_packages=["cotidia"],
     include_package_data=True,
-    install_requires=[
-        "django-form-utils==1.0.*",
-        "django-filter==1.0.*",
-        "django-anymail==1.0.*"
-    ],
+    install_requires=["django-form-utils==1.0.*", "django-anymail==1.0.*"],
     classifiers=[
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Topic :: Software Development',
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development",
     ],
 )
